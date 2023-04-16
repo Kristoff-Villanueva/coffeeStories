88c6022c173aef3f898c65af1ad91811c234904c
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2023-04-16 23:23:41 +0800

    change homepage h1 details

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home(props) {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Coffee Connoisseur</title>
+				<title>Coffee Stories</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main}>

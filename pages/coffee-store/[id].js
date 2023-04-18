11d43a4affe7678c2a4900e9ed95525a9c0da1ab
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2023-04-18 09:34:14 +0800

    add getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,33 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export function getStaticProps(staticProps) {
+	const params = staticProps.params;
+	return {
+		props: {
+			coffeeStore: coffeeStoresData.find((coffeeStore) => {
+				return coffeeStore.id === 0;
+			}),
+		},
+	};
+}
+
+export function getStaticPaths() {
+	return {
+		paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+	};
+}
 
 const CoffeeStore = () => {
 	const router = useRouter();
-	return <div>Coffee Store Page</div>;
+	const id = router.query.id;
+	console.log(router);
+	return (
+		<div>
+			{`Coffee Store Page ${id}`} <Link href="/">Back to Home</Link>
+		</div>
+	);
 };
 
 export default CoffeeStore;
